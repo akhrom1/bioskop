f856a8726eb14aed9fe2772eab5c5351c212d654
R Akhrom Darmawan <rahmat.darmawan16@student.uisi.ac.id>
2026-01-09 22:04:09 +0700

    UDPATE

--- database/db.go
+++ database/db.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
@@ -17,17 +16,18 @@ func Connect() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	dsn := os.Getenv("DATABASE_URL")
 
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	// host := os.Getenv("DB_HOST")
+	// port := os.Getenv("DB_PORT")
+	// user := os.Getenv("DB_USER")
+	// password := os.Getenv("DB_PASSWORD")
+	// dbname := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
-	)
+	// dsn := fmt.Sprintf(
+	// 	"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+	// 	host, port, user, password, dbname,
+	// )
 
 
 	DB, err = sql.Open("postgres", dsn)
